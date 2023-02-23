0ead990e6cddfdc687614dbd5d31e0c7aa0fef28
momocoder14 <jammeh2929@gmail.com>
2023-02-22 16:29:42 -0800

    Everything good yeah

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,9 +3,9 @@
 /**
  * print_sign - prints the sign of a number
  * @n: the int to check
- * Return: 1 and print + if n is graeter than zero
- * 0 and points 0 if n is zero
- * -1 and prints, if n is less than zero
+ * Return: 1 and prints + if n is greater than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
@@ -16,8 +16,8 @@ int print_sign(int n)
 	} else if (n == 0)
 	{
 		_putchar(48);
-		return (1);
-	} esle if (n < 0)
+		return (0);
+	} else if (n < 0)
 	{
 		_putchar('-');
 	}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * times_table - prints the 9 times table, starting with 0
@@ -7,7 +7,7 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 10; j++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
@@ -16,19 +16,21 @@ void times_table(void)
 			{
 				_putchar(k + '0');
 			}
-		}	if (k < 10 && j != 0)
+
+			if (k < 10 && j != 0)
 			{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(k + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
 			} else if (k >= 10)
 			{
-			_putchar(',');
-			_putchar(' ');
-			_putchar((k / 10) + '0');
-			_putchar((k % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
